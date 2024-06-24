2cbf3d79805727f153cedcbe2059eff3d1d701b4
Teages <Teages@teages.xyz>
2024-06-24 13:23:11 +0800

    feat: add command to generate migration

---
+++ prisma/migrations/0001_3f2e555a.sql
@@ -0,0 +1,2 @@
+-- CreateIndex
+CREATE INDEX "todos_user_id_idx" ON "todos"("user_id");
---
+++ scripts/db-generate.ts
@@ -0,0 +1,72 @@
+import fs from 'node:fs/promises'
+import { exec } from 'node:child_process'
+import { webcrypto } from 'node:crypto'
+import { PrismaClient } from '@prisma/client'
+
+async function main() {
+  const prisma = new PrismaClient({ datasourceUrl: `file:./tmp.db` })
+  await prisma.$connect()
+
+  // load all migrations to the database
+  const existingMigrations = await scanMigrations()
+  for (const migration of existingMigrations) {
+    const { content } = migration
+    const statements = content.split(/--\*/).filter(Boolean)
+
+    for (const stmt of statements) {
+      await prisma.$executeRawUnsafe(stmt.trim())
+    }
+  }
+  await prisma.$disconnect()
+
+  // generate the next filename
+  const nextId = existingMigrations.length
+  const randomStr = nextId === 0
+    ? 'init'
+    : webcrypto.randomUUID().split('-')[0]
+  const nextFilename = `${String(nextId).padStart(4, '0')}_${randomStr}.sql`
+
+  // run prisma generate
+  const output = (await run([
+    'npx prisma migrate diff',
+    '--from-url file:./prisma/tmp.db',
+    '--to-schema-datamodel ./prisma/schema.prisma',
+    '--script'
+  ].join(' '))).trim()
+
+  // check if generated
+  if (output.split('\n').length <= 1) {
+    console.log(output)
+    console.log(`No changes detected.`)
+  }
+  else {
+    console.log('Generated migration:')
+    output.split('\n').filter(o => o.startsWith('--')).forEach(o => console.log(o))
+    await fs.writeFile(`./prisma/migrations/${nextFilename}`, `${output}\n`, 'utf-8')
+    console.log(`Migration saved to: ${nextFilename}`)
+  }
+
+  // clean up
+  await fs.unlink('./prisma/tmp.db')
+}
+
+main()
+
+async function scanMigrations() {
+  const migrations = await fs.readdir('./prisma/migrations')
+  return Promise.all(migrations.map(async filename => ({
+    filename,
+    content: await fs.readFile(`./prisma/migrations/${filename}`, 'utf-8')
+  })))
+}
+
+function run(cmd: string) {
+  return new Promise<string>((resolve) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.warn(error)
+      }
+      resolve(stdout ? stdout : stderr)
+    })
+  })
+}
